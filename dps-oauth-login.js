57b0a86f6dc0d79a42217fa172a51a8ca3efa5a5
Charlie Wagner <charlie.wagner@6dglobal.com>
2015-07-20 17:32:01 -0400

    changed strings to use ' only, instead of both " and '

--- dps-oauth-login.js
+++ dps-oauth-login.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
 {
@@ -9,8 +9,8 @@
 }
 */
 function OAuthLogin(options) {
-  if (typeof options.authUrl === "undefined") {
-    options.authUrl = "https://entitlement-server.6dglobalcloud.com";
+  if (typeof options.authUrl === 'undefined') {
+    options.authUrl = 'https://entitlement-server.6dglobalcloud.com';
   }
   function errorCallback(error) {
     console.log(error);
@@ -46,23 +46,22 @@ function OAuthLogin(options) {
     });
   }
   function OAuthStart() {
-    var loginUrl = options.authURL + '/login/with/' + options.service;
-    loginUrl += "?appId=" +encodeURIComponent(options.clientId);
-    loginUrl += "&redirect_uri=" +options.redirectURI;
+    var loginUrl= options.authURL + '/login/with/' + options.service;
+    loginUrl += '?appId=' + encodeURIComponent(options.clientId);
+    loginUrl += '&redirect_uri=' + options.redirectURI;
     adobeDPS.dialogService.open(loginUrl);
-    console.log('started');
   }
   function OAuthResponse(response) {
     adobeDPS.dialogService.close();
-    if (response.hasOwnProperty("timeout")) {
+    if (response.hasOwnProperty('timeout')) {
       adobeDPS.oauthRedirectService.stopListening(function() {}, function() {});
-      console.log("the authentication request timed out");
+      console.log('The request timed out');
     }
-    else if (response.hasOwnProperty("error")) {
+    else if (response.hasOwnProperty('error')) {
       adobeDPS.oauthRedirectService.stopListening(function() {}, function() {});
-      console.log("the authentication server returned error");
+      console.log('The server returned an error');
     } else {
-      // the authentication server returned success
+      // success
       adobeDPS.authenticationService.login(null, null, response.auth_token);
     }
   }
